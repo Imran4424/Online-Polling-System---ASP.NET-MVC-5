02f50c5a83f952eef2bbe102c4e45f12dd2147b6
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-15 07:24:29 +0600

    polling system

---
+++ OnlinePollingSystem/Models/Poll.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlinePollingSystem.Models
+{
+    public class Poll
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public DateTime PostDate { get; set; }
+
+        [Required]
+        public string PostedBy { get; set; }
+
+        [Required]
+        public string UserIdentity { get; set; }
+    }
+}
