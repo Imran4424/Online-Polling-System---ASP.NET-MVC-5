8db1726fee9075c9e8035638ca6ec439860e173f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-15 06:57:48 +0600

    polling system

--- OnlinePollingSystem/Controllers/AccountController.cs
+++ OnlinePollingSystem/Controllers/AccountController.cs
@@ -151,7 +151,13 @@ namespace OnlinePollingSystem.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name,
+                    Country = model.Country
+                };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
