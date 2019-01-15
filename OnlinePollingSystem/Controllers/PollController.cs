308c861ad91e191cd7fc847e0f204102d25fdf78
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-15 17:32:34 +0600

    polling system

--- OnlinePollingSystem/Controllers/PollController.cs
+++ OnlinePollingSystem/Controllers/PollController.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+using OnlinePollingSystem.Models;
 
 namespace OnlinePollingSystem.Controllers
 {
     public class PollController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public PollController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         // GET: Poll
         public ActionResult Index()
         {
